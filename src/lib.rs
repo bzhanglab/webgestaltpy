e034e258195c789a6ae6c8dc4b483e1ec0510b42
John Elizarraras <elizarrj27@gmail.com>
2023-09-21 11:12:51 -0500

    add ORA results

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::time::Instant;
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
 use webgestalt_lib::methods::gsea::FullGSEAResult;
+use webgestalt_lib::methods::ora::ORAResult;
 
 fn result_to_dict(obj: FullGSEAResult, py: Python) -> &PyDict {
     let res = vec![
@@ -12,6 +13,17 @@ fn result_to_dict(obj: FullGSEAResult, py: Python) -> &PyDict {
         ("es".to_object(py), obj.es.to_object(py)),
         ("nes".to_object(py), obj.nes.to_object(py)),
         ("leading_edge".to_object(py), obj.leading_edge.to_object(py)),
+    ]
+    .to_object(py);
+    PyDict::from_sequence(py, res).unwrap()
+}
+
+fn ora_result_to_dict(obj: ORAResult, py: Python) -> &PyDict {
+    let res = vec![
+        ("set".to_object(py), obj.set.to_object(py)),
+        ("p".to_object(py), obj.p.to_object(py)),
+        ("fdr".to_object(py), obj.fdr.to_object(py)),
+        ("overlap".to_object(py), obj.overlap.to_object(py)),
     ]
     .to_object(py);
     PyDict::from_sequence(py, res).unwrap()
@@ -39,7 +51,24 @@ fn gsea_from_files(py: Python, gmt: String, rank: String) -> PyResult<Vec<&PyDic
         webgestalt_lib::methods::gsea::gsea(gene_list.unwrap(), gmt.unwrap());
     let new_res: Vec<&PyDict> = res.into_iter().map(|x| result_to_dict(x, py)).collect();
     let duration = start.elapsed();
-    println!("New Hash\nTime took: {:?}", duration);
+    println!("GSEA\nTime took: {:?}", duration);
+    Ok(new_res)
+}
+
+#[pyfunction]
+fn ora_from_files(
+    py: Python,
+    gmt_path: String,
+    gene_list_path: String,
+    reference_list_path: String,
+) -> PyResult<Vec<&PyDict>> {
+    let (gmt, gene_list, reference) =
+        webgestalt_lib::readers::read_ora_files(gmt_path, gene_list_path, reference_list_path);
+    let start = Instant::now();
+    let res: Vec<ORAResult> = webgestalt_lib::methods::ora::get_ora(&gene_list, &reference, gmt);
+    let new_res: Vec<&PyDict> = res.into_iter().map(|x| ora_result_to_dict(x, py)).collect();
+    let duration = start.elapsed();
+    println!("ORA\nTime took: {:?}", duration);
     Ok(new_res)
 }
 
@@ -47,5 +76,6 @@ fn gsea_from_files(py: Python, gmt: String, rank: String) -> PyResult<Vec<&PyDic
 #[pymodule]
 fn webgestaltpy(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(gsea_from_files, m)?)?;
+    m.add_function(wrap_pyfunction!(ora_from_files, m)?)?;
     Ok(())
 }
--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 import webgestaltpy
 
 x = webgestaltpy.gsea_from_files("data/ktest.gmt", "data/test.rnk")
-print("There are {0} pathways with significant p values".format(len([i for i in x if i["p"] < 0.05])))
+print(
+    "GSEA: There are {0} pathways with significant p values".format(
+        len([i for i in x if i["fdr"] < 0.05])
+    )
+)
+
+y = webgestaltpy.ora_from_files(
+    "data/ktest.gmt", "data/genelist.txt", "data/reference.txt"
+)
+print(
+    "ORA: There are {0} pathways with significant fdr values".format(
+        len([i for i in y if i["fdr"] < 0.05])
+    )
+)
